78314509477ab2277defb0fb8fbd51fe934a08bd
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-06-11 14:15:44 +0100

    ADD: osINT

---
+++ src/modules/osint.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.art import *
+from src.print import *
+from src.utilities import *
+
+
+class osintInstagram:
+    def __init__(self):
+        banner()
